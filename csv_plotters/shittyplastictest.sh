b28389ffa2003a0054b79aa48f132eb1d0e48976
Theodore <theodorez888@gmail.com>
2024-12-06 18:19:25 +0900

    add

---
+++ csv_plotters/shittyplastictest.sh
@@ -0,0 +1,17 @@
+python3 CSVisualizer.py << EOF
+range
+confidence
+8
+no
+~/debug_bags/new_dist_sensor_bag/spinningnerd.csv
+~/debug_bags/new_dist_sensor_bag/spinningnerd_left_redo.csv
+~/debug_bags/new_dist_sensor_bag/shittyplastictest.csv
+~/debug_bags/new_dist_sensor_bag/shittyplastic2.csv
+~/debug_bags/new_dist_sensor_bag/frontbin.csv
+~/debug_bags/new_dist_sensor_bag/leftbin.csv
+~/debug_bags/new_dist_sensor_bag/rearbin.csv
+~/debug_bags/new_dist_sensor_bag/rightbin.csv
+pass
+logline -0.78 1000 4.5 2.1
+end
+EOF
